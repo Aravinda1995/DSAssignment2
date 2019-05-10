6e6cf9c630eeb8dd9050a0f9221951971e819626
Aravinda1995 <aravindakulasooriya1995@gmail.com>
2019-05-10 18:59:19 +0530

    Added a basic header :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This my first React app</h1>
-    </div>
+      <div>
+        <Header/>
+      </div>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Components.css'
+
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>Welcome to Train Bookings</h1>
+        </div>
+    )
+};
+
+export default Header;
